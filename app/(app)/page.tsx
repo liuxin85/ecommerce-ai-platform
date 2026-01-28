8a1cf2f05688da95a84ec884723b4eb0b993506b
liuxin85 <liuxin85@gmail.com>
2026-01-29 00:14:35 +0800

    feat: add shadcn ui

---
+++ app/(app)/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      {/* Featured Products Carousel */}
+      <Button>clicke me</Button>
+      <Button variant="default">clicke me</Button>
+      <Button variant="destructive">clicke me</Button>
+      <Button variant="ghost">clicke me</Button>
+
+      {/* Page Banner */}
+
+      {/* Category Tiles */}
+
+      {/* Products Section */}
+    </div>
+  );
+}
